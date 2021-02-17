7923e3671d9ddab47c0bc1fc79747cf57bd1faaf
John-v77 <ionut_vasile85@hotmail.com>
2021-02-17 17:19:41 -0500

    Importing  data

--- src/components/QuoteDetails.js
+++ src/components/QuoteDetails.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {appl} from './data';
 
 function QouteDetails(props) {
+
+    let [stock, setStock] = useState(appl)
+
+    
+    const token = 'pk_695271cfa88a4f01969c642eb1576b3f'
+    axios.get(`https://cloud.iexapis.com/stable/stock/aapl/quote?token=${token}`)
+        .then( dataZ => {
+            console.log(dataZ)
+            setStock(dataZ)
+        })
+
+// useEffect(() => {
+//     axios.get(`https://cloud.iexapis.com/stable/stock/aapl/quote?token=${token}`)
+//         .then( dataZ => {
+//             console.log(dataZ)
+//         })
+
+//         }, [])
+
+
+        const displayData = () => {
+           return Object.keys(stock).map(eachItem => {
+                console.log(eachItem)
+                return(
+                    <div>
+                       <li>
+                            {eachItem} : {stock[eachItem]}
+                       </li>
+                    </div>
+                )
+            })
+
+        }
+
+
     return (
         <div class="component-container">
-                QuoteDetails
+        {stock.companyName}
+                {displayData()}
         </div>
     );
 }
